1e083a2a84f8a956757de3b2fc094b46a18cbf87
luisOlvera <luis.olvera@tralix.com>
2015-04-22 18:08:00 -0500

    test modificado para convertir el 1 en I

--- src/test/numerosRomanosTest.java
+++ src/test/numerosRomanosTest.java
@@ -8,9 +8,9 @@ import static org.junit.Assert.*;
 public class numerosRomanosTest {
 
     @org.junit.Test
-    public void Decimal_1_result_null() throws Exception {
+    public void Decimal_1_result_I() throws Exception {
         numerosRomanos numRomano = new numerosRomanos();
         numRomano.obtenDecimalEnRomano(1);
-        Assert.assertEquals(null, numRomano.obtenDecimalEnRomano(1));
+        Assert.assertEquals("I", numRomano.obtenDecimalEnRomano(1));
     }
 }
