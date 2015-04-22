5145624039d06f4a6ee47d4bcc13e3fc843c41bf
luisOlvera <luis.olvera@tralix.com>
2015-04-22 18:25:02 -0500

    Refactor de pruebas y clase

--- src/test/numerosRomanosTest.java
+++ src/test/numerosRomanosTest.java
@@ -7,16 +7,24 @@ import static org.junit.Assert.*;
 
 public class numerosRomanosTest {
 
+    numerosRomanos numRomano ;
+    @org.junit.Before
+    public  void setup (){
+        numRomano = new numerosRomanos();
+    }
+
     @org.junit.Test
     public void Decimal_1_result_I() throws Exception {
-        numerosRomanos numRomano = new numerosRomanos();
-        Assert.assertEquals("I", numRomano.obtenDecimalEnRomano(1));
+        assertConversionDeDecimal(1,"I");
     }
 
     @org.junit.Test
     public void Decimal_2_result_II() throws Exception {
-        numerosRomanos numRomano = new numerosRomanos();
-        Assert.assertEquals("II", numRomano.obtenDecimalEnRomano(2));
+        assertConversionDeDecimal(2,"II");
+    }
+
+    public void assertConversionDeDecimal( Integer Deciaml, String numeroRomano){
+        Assert.assertEquals(numeroRomano, numRomano.obtenDecimalEnRomano(Deciaml));
     }
 
 }
